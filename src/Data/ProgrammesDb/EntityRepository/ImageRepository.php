d9d3d703ccae9754d9e106aeefdaa7acdc8e694d
Bjorn Twachtmann <bjorn.twachtmann@bbc.co.uk>
2018-11-19 12:15:21 +0000

    Add queries for gallery images

--- src/Data/ProgrammesDb/EntityRepository/ImageRepository.php
+++ src/Data/ProgrammesDb/EntityRepository/ImageRepository.php
@@ -15,4 +15,17 @@ class ImageRepository extends EntityRepository
 
         return $qb->getQuery()->getOneOrNullResult(Query::HYDRATE_ARRAY);
     }
+
+    public function findByGroup(string $groupId): ?array
+    {
+        $qb = $this->createQueryBuilder('image')
+            ->innerJoin('ProgrammesPagesService:Membership', 'membership', Query\Expr\Join::WITH, 'membership.memberImage = image')
+            ->where('IDENTITY(membership.group) = :group_id')
+            ->setParameter('group_id', $groupId);
+        return $qb->getQuery()->getResult(Query::HYDRATE_ARRAY);
+    }
 }
+
+
+
+
